b8aa8ca121aa74db18398ef12d641f7d9a7d4b3d
Micah Newsum <micah.newsum@glidewelldental.com>
2026-04-29 07:12:05 -0700

    feat: Implement Evaluate logic

--- src/main/chapter1_3/Evaluate.java
+++ src/main/chapter1_3/Evaluate.java
@@ -1,7 +1,34 @@
 package main.chapter1_3;
 
+import edu.princeton.cs.algs4.Stack;
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+
 public class Evaluate {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Stack<String> ops = new Stack<>();
+        Stack<Double> vals = new Stack<>();
+
+        while (!StdIn.isEmpty()) {
+            String s = StdIn.readString();
+            if (s.equals("(")) ;
+            else if (s.equals("+")) ops.push(s);
+            else if (s.equals("-")) ops.push(s);
+            else if (s.equals("*")) ops.push(s);
+            else if (s.equals("/")) ops.push(s);
+            else if (s.equals("sqrt")) ops.push(s);
+            else if (s.equals(")")) {
+                Double val = vals.pop();
+                String op  = ops.pop();
+                if (op.equals("+")) val = val + vals.pop();
+                if (op.equals("-")) val = val - vals.pop();
+                if (op.equals("*")) val = val * vals.pop();
+                if (op.equals("/")) val = val / vals.pop();
+                if (op.equals("sqrt")) val = Math.sqrt(vals.pop());
+                vals.push(val);
+            }
+            else vals.push(Double.parseDouble(s));
+        }
+        StdOut.println(vals.pop());
     }
 }
